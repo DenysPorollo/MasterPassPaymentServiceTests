0d5082082c8bb2a92bc0b650d5cc4cba81eda74a
via.tkachuk <via.tkachuk@W-KV-TYCH1O-025>
2018-11-27 10:04:33 +0200

    добавлены тесты на Purchase

--- MasterPassPaymentServiceTests/InitialData.cs
+++ MasterPassPaymentServiceTests/InitialData.cs
@@ -15,15 +15,26 @@ namespace MasterPassPaymentServiceTests
         public static int       filialId        = 1998;
         public static string    frNumber        = "NF00000000";
         public static string    pos_ip          = "192.168.0.106";
-        public static int       localChequeId   = 28100;
+        public static int       localChequeId   = 28102;
         public static int       zId             = -1;
         public static string    posName         = "\u041a\u0430\u0441\u0441\u0430 N  6";
         public static string    uuid            = "1308b38c-62eb-11e6-a591-9f9dbd6df78d";
 
         //Не верные данные
-
         public static int zId_wrong = 1234567890;
         public static string frNumber_wrong = "0000000000";
         public static string pos_ip_wrong = "127.0.0.1";
+
+        //Не корректныйе
+        public static string guid_wrong = "8F322CAA-EF25-4D52-992B-1754343B67B0";
+        public static string uniqueId_wrong = "0639DB14E9F6906F54D722F474A0952E52FA4F60292B13F23184D11A7537E853";
+        public static double amount_plus1cop = 76.87;
+
+        public static double amount_plus1hrn = 77.87;
+
+        public static int filialId_wrong = 1999;
+        //public static int localChequeId = 28102;
+        public static string posName_wrong = "\u041a\u0430\u0441\u0441\u0430 B  6";
+        public static string uuid_wrong = "1308b38c-62eb-11e6-a591-9f9dbd6df78s";
     }
 }
--- MasterPassPaymentServiceTests/PurchaseTests.cs
+++ MasterPassPaymentServiceTests/PurchaseTests.cs
@@ -18,21 +18,21 @@ namespace MasterPassPaymentServiceTests
         {
 
             //Arrange
-            string guid         = InitialData.guid;
-            string uniqueId     = InitialData.uniqueId;
-            double amount       = InitialData.amount;
-            int filialId        = InitialData.filialId;
-            string frNumber     = InitialData.frNumber_wrong;
-            string pos_ip       = InitialData.pos_ip_wrong;
-            int localChequeId   = InitialData.localChequeId;
-            int zId             = InitialData.zId_wrong;
-            string posName      = InitialData.posName;
-            string uuid         = InitialData.uuid;
+            string guid = InitialData.guid;
+            string uniqueId = InitialData.uniqueId;
+            double amount = InitialData.amount;
+            int filialId = InitialData.filialId;
+            string frNumber = InitialData.frNumber_wrong;
+            string pos_ip = InitialData.pos_ip_wrong;
+            int localChequeId = InitialData.localChequeId;
+            int zId = InitialData.zId_wrong;
+            string posName = InitialData.posName;
+            string uuid = InitialData.uuid;
 
             //expected
             Int32 expected_errorCode = 0;
             string expected_errorMessage = "OK";
-            string expected_terminalId = "QR"+ filialId;
+            string expected_terminalId = "QR" + filialId;
 
             //Act
             dynamic result = JsonConvert.DeserializeObject(SwaggerMethodsMasterPassPaymentService.Purchase(guid, uniqueId, filialId, posName, pos_ip, frNumber, zId, localChequeId, amount, uuid));
@@ -43,6 +43,132 @@ namespace MasterPassPaymentServiceTests
             Assert.AreEqual(expected_errorMessage, (string)result.errorMessage);
             Assert.AreEqual(expected_errorCode, (Int32)result.errorCode);
             Assert.AreEqual(expected_terminalId, (string)result.terminalId);
+
+        }
+
+        [TestMethod]
+        public void Purchase_WRONG_guid_RETURN_ERROR()
+        {
+
+            //Arrange
+            string guid = InitialData.guid_wrong;
+            string uniqueId = InitialData.uniqueId;
+            double amount = InitialData.amount;
+            int filialId = InitialData.filialId;
+            string frNumber = InitialData.frNumber;
+            string pos_ip = InitialData.pos_ip;
+            int localChequeId = 28103;
+            int zId = InitialData.zId;
+            string posName = InitialData.posName;
+            string uuid = InitialData.uuid;
+
+            //expected
+            Int32 expected_errorCode = 3;
+            string expected_errorMessage = "msisdn not found for specified guid";
+            string expected_terminalId = "QR" + filialId;
+
+            //Act
+            dynamic result = JsonConvert.DeserializeObject(SwaggerMethodsMasterPassPaymentService.Purchase(guid, uniqueId, filialId, posName, pos_ip, frNumber, zId, localChequeId, amount, uuid));
+
+
+            //Assert
+            Assert.AreEqual(expected_errorCode, (Int32)result.errorCode);
+            Assert.AreEqual(expected_errorMessage, (string)result.errorMessage);
+
+
+        }
+
+        [TestMethod]
+        public void Purchase_WRONG_uniqueId_RETURN_ERROR()
+        {
+
+            //Arrange
+            string guid = InitialData.guid;
+            string uniqueId = InitialData.uniqueId_wrong;
+            double amount = InitialData.amount;
+            int filialId = InitialData.filialId;
+            string frNumber = InitialData.frNumber;
+            string pos_ip = InitialData.pos_ip;
+            int localChequeId = 28106;
+            int zId = InitialData.zId;
+            string posName = InitialData.posName;
+            string uuid = InitialData.uuid;
+
+            //expected
+            Int32 expected_errorCode = 3;
+            string expected_errorMessage = "msisdn not found for specified guid";
+            string expected_terminalId = "QR" + filialId;
+
+            //Act
+            dynamic result = JsonConvert.DeserializeObject(SwaggerMethodsMasterPassPaymentService.Purchase(guid, uniqueId, filialId, posName, pos_ip, frNumber, zId, localChequeId, amount, uuid));
+
+
+            //Assert
+            Assert.AreEqual(expected_errorCode, (Int32)result.errorCode);
+            Assert.AreEqual(expected_errorMessage, (string)result.errorMessage);
+
+        }
+
+        [TestMethod]
+        public void Purchase_WRONG_amount_plus1cop_RETURN_ERROR()
+        {
+
+            //Arrange
+            string guid = InitialData.guid;
+            string uniqueId = InitialData.uniqueId;
+            double amount = InitialData.amount_plus1cop;
+            int filialId = InitialData.filialId;
+            string frNumber = InitialData.frNumber;
+            string pos_ip = InitialData.pos_ip;
+            int localChequeId = 28103;
+            int zId = InitialData.zId;
+            string posName = InitialData.posName;
+            string uuid = InitialData.uuid;
+
+            //expected
+            Int32 expected_errorCode = 3;
+            string expected_errorMessage = "msisdn not found for specified guid";
+            string expected_terminalId = "QR" + filialId;
+
+            //Act
+            dynamic result = JsonConvert.DeserializeObject(SwaggerMethodsMasterPassPaymentService.Purchase(guid, uniqueId, filialId, posName, pos_ip, frNumber, zId, localChequeId, amount, uuid));
+
+
+            //Assert
+            Assert.AreEqual(expected_errorCode, (Int32)result.errorCode);
+            Assert.AreEqual(expected_errorMessage, (string)result.errorMessage);
+
+        }
+
+
+        [TestMethod]
+        public void Purchase_WRONG_amount_plus1hrn_RETURN_ERROR()
+        {
+
+            //Arrange
+            string guid = InitialData.guid;
+            string uniqueId = InitialData.uniqueId;
+            double amount = InitialData.amount_plus1hrn;
+            int filialId = InitialData.filialId;
+            string frNumber = InitialData.frNumber;
+            string pos_ip = InitialData.pos_ip;
+            int localChequeId = 28104;
+            int zId = InitialData.zId;
+            string posName = InitialData.posName;
+            string uuid = InitialData.uuid;
+
+            //expected
+            Int32 expected_errorCode = 0;
+            string expected_errorMessage = "msisdn not found for specified guid";
+            string expected_terminalId = "QR" + filialId;
+
+            //Act
+            dynamic result = JsonConvert.DeserializeObject(SwaggerMethodsMasterPassPaymentService.Purchase(guid, uniqueId, filialId, posName, pos_ip, frNumber, zId, localChequeId, amount, uuid));
+
+
+            //Assert
+            Assert.AreEqual(expected_errorCode, (Int32)result.errorCode);
+            Assert.AreEqual(expected_errorMessage, (string)result.errorMessage);
 
         }
     }
